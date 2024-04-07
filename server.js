0fb731310c998bef2621ab8c8848669eded6ca75
babacar <babacar.diakite01@gmail.com>
2024-04-07 15:50:00 -0400

    testing

--- server.js
+++ server.js
@@ -12,6 +12,46 @@ mongoose
   .then(() => {
     console.log('connection successful');
   });
+/// CREATING OUR SCHEMA
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    require: [true, 'A tour must have a rating'],
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    require: [true, 'A tour must a price'],
+  },
+});
+
+/// CREATING OUR MODEL OUT OF OUR SCHEMA
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+//// CREATING AN INSTANCE OF OUR MODEL
+const tesTour = new Tour({
+  name: 'Rock clamer',
+  rating: 6.3,
+  price: 600,
+});
+
+/// SAVING OUR OBJECT IN THE MONGODB
+
+tesTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log('ERRO 🛑⛔🛑', err);
+  });
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
