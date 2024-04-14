68843db96248c8d0fd437fd160bf859b73d45c4a
babacar <babacar.diakite01@gmail.com>
2024-04-14 00:24:12 -0400

    virtual properties

--- model/tourModel.js
+++ model/tourModel.js
@@ -1,77 +1,85 @@
 const mongoose = require('mongoose');
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true,
-    trim: true,
-  },
-  duration: {
-    type: Number,
-    required: [true, 'A tour must have a duration'],
-  },
-  maxGroupSize: {
-    type: Number,
-    required: [true, 'A tour must have a maxgroup size'],
-  },
-  difficulty: {
-    type: String,
-    trim: true,
-    required: [true, 'A tour must have a difficulty'],
-  },
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'A tour must have a name'],
+      unique: true,
+      trim: true,
+    },
+    duration: {
+      type: Number,
+      required: [true, 'A tour must have a duration'],
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'A tour must have a maxgroup size'],
+    },
+    difficulty: {
+      type: String,
+      trim: true,
+      required: [true, 'A tour must have a difficulty'],
+    },
 
-  ratingsAverage: {
-    type: Number,
-    default: 4.5,
-    min: [1, 'Rating must be above 1.0'],
-    max: [5, 'Rating must be below 5.0'],
-  },
-  ratingsQuantity: {
-    type: Number,
-    default: 0,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price'],
-  },
-  priceDiscount: {
-    type: Number,
-    // validate: {
-    //   validator: function(val) {
-    //     // this only points to current doc on NEW document creation
-    //     return val < this.price;
-    //   },
-    //   message: 'Discount price ({VALUE}) should be below regular price'
+    ratingsAverage: {
+      type: Number,
+      default: 4.5,
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be below 5.0'],
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: [true, 'A tour must have a price'],
+    },
+    priceDiscount: {
+      type: Number,
+      // validate: {
+      //   validator: function(val) {
+      //     // this only points to current doc on NEW document creation
+      //     return val < this.price;
+      //   },
+      //   message: 'Discount price ({VALUE}) should be below regular price'
+      // }
+    },
+    summary: {
+      type: String,
+      trim: true,
+      required: [true, 'A tour must have a description'],
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    imageCover: {
+      type: String,
+      required: [true, 'A tour must have a cover image'],
+    },
+    images: [String],
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false,
+    },
+    startDates: [Date],
+    // secretTour: {
+    //   type: Boolean,
+    //   default: false
     // }
   },
-  summary: {
-    type: String,
-    trim: true,
-    required: [true, 'A tour must have a description'],
-  },
-  description: {
-    type: String,
-    trim: true,
-  },
-  imageCover: {
-    type: String,
-    required: [true, 'A tour must have a cover image'],
-  },
-  images: [String],
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-    select: false,
-  },
-  startDates: [Date],
-  // secretTour: {
-  //   type: Boolean,
-  //   default: false
-  // }
-});
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
 
 /// CREATING OUR MODEL OUT OF OUR SCHEMA
-
+tourSchema.virtual('durationWeek').get(function () {
+  return this.duration / 7;
+});
 const Tour = mongoose.model('Tour', tourSchema);
 
 module.exports = Tour;
