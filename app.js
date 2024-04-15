a9033372e1aa1da9094a4a5d9466372d7c25cf6a
babacar <babacar.diakite01@gmail.com>
2024-04-15 19:19:52 -0400

    error implimenting done

--- app.js
+++ app.js
@@ -12,19 +12,28 @@ if (process.env.NODE_ENV === 'developement') {
 
 app.use(express.json());
 
-// app.use((req, res, next) => {
-//   req.requestTime = new Date().toISOString();
-//   console.log(req.requestTime);
-//   next();
-// });
-// app.use((req, res, next) => {
-//   console.log(`hello from middleware 👋👋`);
-//   next();
-// });
-
 // READING FILE
 
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/tours', tourRouter);
+
+app.all('*', (req, res, next) => {
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't find ${req.originalUrl} on this server`,
+  // });
+  const err = new Error(`Can't find ${req.originalUrl} on this server`);
+  err.status = 'fail';
+  err.statusCode = 404;
+  next(err);
+});
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'Error';
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
 /// SERVER
 module.exports = app;
