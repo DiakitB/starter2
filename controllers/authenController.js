c73101f76fb7e7b48181ffa8c0328883524a5fc6
babacar <babacar.diakite01@gmail.com>
2024-04-17 21:44:18 -0400

    sign up function implimented

--- controllers/authenController.js
+++ controllers/authenController.js
@@ -1,10 +1,21 @@
 const User = require('../model/userModel');
-console.log(User, 'THIS IS THE USER');
+
+const jwt = require('jsonwebtoken');
+console.log(jwt);
 exports.signup = async (req, res) => {
   try {
-    const newUser = await User.create(req.body);
+    const newUser = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      confirmPassword: req.body.confirmPassword,
+    });
+
+    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
+    console.log(token);
     res.status(201).json({
       status: 'success',
+      token,
       data: {
         user: newUser,
       },
